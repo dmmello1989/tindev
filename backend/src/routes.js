f14215df9e2602e5c7205f843831c35bd6223606
Daniel Mello <dmmello1989@gmail.com>
2020-03-10 13:43:51 -0300

    resolve merge conflict

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,8 +10,4 @@ routes.post("/devs", DevController.store);
 routes.post("/devs/:devId/likes", LikeController.store);
 routes.post("/devs/:devId/dislikes", DislikeController.store);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a388ce966877eb0e28602a35284aa4323771aa3a
 module.exports = routes;
